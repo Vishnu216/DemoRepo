c123ab7f20299ccf97ae01c2b7ec415241a77b86
Anandu Pushpalal <anandupushpalal77@gmail.com>
2020-11-14 17:15:19 +0530

    added view model

---
+++ models/view.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const viewSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true,
+   },
+   email: {
+      type: String,
+      required: true,
+      unique: true,
+   },
+})
+
+const User = mongoose.model('user', viewSchema)
+
+module.exports = User
